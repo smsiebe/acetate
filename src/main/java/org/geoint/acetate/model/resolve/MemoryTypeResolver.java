3bb3cd41e0d4004917df5c9876944fd35bb7a244
Steve Siebert <steve@geoint.org>
2016-04-11 15:51:32 -0400

    rename package for cleaner api

---
+++ src/main/java/org/geoint/acetate/model/resolve/MemoryTypeResolver.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright 2016 geoint.org.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.geoint.acetate.model.resolve;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Optional;
+import org.geoint.acetate.model.DomainType;
+
+/**
+ * Simple in-memory type resolver.
+ * <p>
+ * The thread safety of this class depends on the underlying collection used.
+ */
+public class MemoryTypeResolver implements TypeResolver {
+
+    private final Collection<DomainType> types;
+
+    /**
+     * Creates a non-thread-safe type in-memory resolver.
+     */
+    public MemoryTypeResolver() {
+        types = new ArrayList<>();
+    }
+
+    /**
+     * Creates a fixed-sized in-memory resolver backed by the provided array.
+     *
+     * @param types types resolved by the resolver
+     */
+    public MemoryTypeResolver(DomainType... types) {
+        this.types = Arrays.asList(types);
+    }
+
+    /**
+     * Uses the provided collection as the source for the domain types.
+     * <p>
+     * If you plan on using this resolver across threads you should use a
+     * concurrent collection here.
+     *
+     * @param types
+     */
+    public MemoryTypeResolver(Collection<DomainType> types) {
+        this.types = types;
+    }
+
+    /**
+     * Returns the types known to the resolver instance.
+     * <p>
+     * This method returns the actual collection used by the resolver, so any
+     * changes made to this collection will impact the resolver. Consider thread
+     * safety here.
+     *
+     * @return resolved types
+     */
+    public Collection<DomainType> getTypes() {
+        return types;
+    }
+
+    @Override
+    public Optional<DomainType> resolve(String namespace, String version,
+            String typeName) {
+        return types.stream()
+                .filter((t) -> t.isType(namespace, version, typeName))
+                .findFirst();
+    }
+
+}
---
+++ src/main/java/org/geoint/acetate/model/resolve/TypeResolver.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2016 geoint.org.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.geoint.acetate.model.resolve;
+
+import java.util.Optional;
+import org.geoint.acetate.model.DomainType;
+
+/**
+ * Provides domain descriptor to type resolution services.
+ * <p>
+ * Implementations must indicate if they are thread-safe or not.
+ *
+ * @author steve_siebert
+ */
+@FunctionalInterface
+public interface TypeResolver {
+
+    /**
+     * Returns the requested type, if known to the resolver.
+     *
+     * @param namespace domain namespace
+     * @param version domain version
+     * @param typeName domain type name
+     * @return domain type, if known to the resolver
+     */
+    Optional<DomainType> resolve(String namespace, String version,
+            String typeName);
+}
